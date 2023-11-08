7d19f65f43df140a439a15a46f3d63aa6d39fd61
GuillermoMML <guillermoml2002@gmail.com>
2023-11-08 12:43:01 +0000

    Adding google material3 and modify layout profile page and adding new colors

--- app/src/main/java/com/example/finalapppamn/MainActivity.kt
+++ app/src/main/java/com/example/finalapppamn/MainActivity.kt
@@ -1,13 +1,7 @@
 package com.example.finalapppamn
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.finalapppamn.ui.theme.FinalAppPamnTheme
-
+import androidx.compose.material3.lightColorScheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,18 +9,5 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FinalAppPamnTheme {
-        Greeting("Android")
-    }
-}
+
