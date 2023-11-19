866e395184fdc8b86909850175dabc6d619b1cc8
GuillermoMML <guillermoml2002@gmail.com>
2023-11-19 19:16:25 +0000

    Adding Login And Register

--- app/src/main/java/com/example/finalapppamn/MainActivity.kt
+++ app/src/main/java/com/example/finalapppamn/MainActivity.kt
@@ -1,4 +1,5 @@
 package com.example.finalapppamn
+
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
@@ -6,35 +7,44 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var bottomNavigationView: BottomNavigationView
+    private var isLoggedIn = false // Change this variable to true when the user logs in
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
 
         bottomNavigationView = findViewById(R.id.bottom_navigation)
-
         bottomNavigationView.setOnItemSelectedListener { menuItem ->
-            when(menuItem.itemId){
+            when (menuItem.itemId) {
                 R.id.menu_item_home -> {
                     replaceFragment(HomePage())
                     true
                 }
+
                 R.id.menu_item_profile -> {
                     replaceFragment(ProfilePage())
                     true
                 }
+
                 R.id.menu_item_settings -> {
                     replaceFragment(HomePage())
                     true
                 }
-                else -> false
+
+                else -> {
+                    // Replace with HomePage fragment if no option is selected
+                    replaceFragment(HomePage())
+                    true
+                }
             }
         }
-        replaceFragment(HomePage())
+
     }
 
-    private fun replaceFragment(fragment: Fragment){
-        supportFragmentManager.beginTransaction().replace(R.id.frame_layout,fragment).commit()
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.frame_layout, fragment).commit()
     }
 
 }
---
+++ app/src/main/java/com/example/finalapppamn/signUpActivity.kt
@@ -0,0 +1,11 @@
+package com.example.finalapppamn
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class signUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_sign_up)
+    }
+}
