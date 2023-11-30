8962dd342ae20d32c2c00fffe4c93ea32aeea4d7
GuillermoMML <guillermoml2002@gmail.com>
2023-11-30 10:21:15 +0000

    Register

--- app/src/main/java/com/example/finalapppamn/MainActivity.kt
+++ app/src/main/java/com/example/finalapppamn/MainActivity.kt
@@ -1,17 +1,12 @@
 package com.example.finalapppamn
 
-import android.content.ContentValues.TAG
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.datastore.preferences.preferencesDataStore
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.Firebase
 import com.google.firebase.firestore.firestore
-val Context.dataStore by preferencesDataStore(name = "USER_PREFERENCES_NAME")
 class MainActivity : AppCompatActivity() {
     private lateinit var bottomNavigationView: BottomNavigationView
     val db = Firebase.firestore
