c0c1517c8876263333bbb8ffeef25f6a47124ed5
GuillermoMML <guillermoml2002@gmail.com>
2023-12-07 16:01:08 +0000

    Login succesfull and intent uid user to the MainActivity

--- app/src/main/java/com/example/finalapppamn/MainActivity.kt
+++ app/src/main/java/com/example/finalapppamn/MainActivity.kt
@@ -1,27 +1,27 @@
 package com.example.finalapppamn
 
-import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.example.finalapppamn.LoginActivity.Companion.EXTRA_USER
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.firestore
-import com.squareup.picasso.Picasso
 
 class MainActivity : AppCompatActivity() {
     private lateinit var bottomNavigationView: BottomNavigationView
     val db = Firebase.firestore
+    // Get the user from the intent
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         replaceFragment(HomePage())
+
+        //Obtenemos el uid del usuario
+        val firebaseUser:String = intent.extras?.getString("EXTRA_USER").orEmpty()
 
         bottomNavigationView = findViewById(R.id.bottom_navigation)
         bottomNavigationView.setOnItemSelectedListener { menuItem ->
