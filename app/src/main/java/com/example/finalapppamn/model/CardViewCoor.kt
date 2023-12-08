3c1ca60e14b2d6f8199144932e85382364cd8f81
GuillermoMML <guillermoml2002@gmail.com>
2023-12-08 11:53:14 +0000

    Updating Map

---
+++ app/src/main/java/com/example/finalapppamn/model/CardViewCoor.kt
@@ -0,0 +1,10 @@
+package com.example.finalapppamn.model
+
+data class CardViewCoor(
+    val title: String,
+    val imageUrl: String,
+    val stars: Int,
+    val price: Int,
+    val latitude: Double?,
+    val longitude: Double?
+)
